ac5cf8f2310b70495e463f202cde4906ddb78f39
Will Christenson <will@wlcr.io>
2025-11-30 10:50:37 -0800

    Update user card

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,10 @@
+import { UserCard } from './UserCard'
+
 export const Header = () => {
   return (
     <header>
       <h1>Welcome to My Website</h1>
+      <UserCard />
     </header>
   )
 }
--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,8 +1,23 @@
 'use client'
 import { Card } from '@radix-ui/themes'
-import { useUser } from '@stackframe/stack'
+import { useUser, UserAvatar } from '@stackframe/stack'
 
 export const UserCard = () => {
   const user = useUser()
-  return <Card>{user ? `Hello, ${user.displayName ?? 'anon'}` : 'You are not logged in'}</Card>
+
+  if (!user) {
+    return (
+      <Card>
+        <UserAvatar />
+        <div>You are not logged in</div>
+      </Card>
+    )
+  }
+
+  return (
+    <Card>
+      <UserAvatar />
+      {`Hello, ${user.displayName ?? 'anon'}`}
+    </Card>
+  )
 }
