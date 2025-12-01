56ff3934c784369765a2f973ed20840e3837e167
Will Christenson <will@wlcr.io>
2025-11-30 23:17:42 -0800

    Fix auth handler layout

--- app/handler/layout.tsx
+++ app/handler/layout.tsx
@@ -1,15 +1,6 @@
-import { stackServerApp } from '@/stack/server';
-import { Header } from '@/components/Header';
-
-const DMLayout = async ({ children }: { children: React.ReactNode }) => {
-  await stackServerApp.getUser({ or: 'redirect' });
-
-  return (
-    <>
-      <Header />
-      <main>{children}</main>
-    </>
-  );
+const HandlerLayout = async ({ children }: { children: React.ReactNode }) => {
+  // Handler routes are used by Stack for auth flows; they must stay unguarded to avoid redirect loops.
+  return <main>{children}</main>;
 };
 
-export default DMLayout;
+export default HandlerLayout;
